559d7b6edf38acb3a2a690d2875a370873f4a43d
James Ward <jimmywardmichael@gmail.com>
2023-07-25 09:37:27 -0500

    init

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+
+    public String firstName;
+    public String lastName;
+
+    public String sayHello(){
+        return  String.format("Hello from %s %s!", firstName, lastName);
+    }
+
+    public static void main(String[] args) {
+        Person rick = new Person();
+        rick.firstName = "Rick";
+        rick.lastName = "Stanchez";
+        System.out.println(rick.sayHello());
+        //prints hello from rick stanchez
+    }
+}
